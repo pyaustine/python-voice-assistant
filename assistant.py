79593c586453c4bee7cff73205b446410ebda053
sphinx-austin <austinjuniour4@gmail.com>
2023-06-26 11:05:45 +0300

    commands incase of unrecognizable speech

--- assistant.py
+++ assistant.py
@@ -30,3 +30,11 @@ def myCommand():
     try:
         command = r.recognize_google(audio).lower()
         talkToMe('You said: ' + command + '\n')
+    
+    #loop back to continue to listen for commands if unrecognizable speech is received
+    except sr.UnknownValueError:
+        talkToMe('Your last command couldn\'t be heard ! I can understand commands like send email, open gmail, open website xyz.com and tell me a joke')
+        #speak.Speak('Your last command couldn\'t be heard')
+        command = myCommand()
+
+    return command
