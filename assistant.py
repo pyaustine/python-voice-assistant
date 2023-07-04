b04a63bceb4e83f8a2aac341f05eeacfeacdf136
sphinx-austin <austinjuniour4@gmail.com>
2023-07-04 19:42:16 +0300

    check weather in proxy

--- assistant.py
+++ assistant.py
@@ -9,20 +9,22 @@ from weather import Weather
 import win32com.client as wincl
 speak = wincl.Dispatch("SAPI.SpVoice")
 
+
 def talkToMe(audio):
-    #"speaks audio passed as argument"
+    # "speaks audio passed as argument"
     print(audio)
     speak.Speak(audio)
 
+
 def myCommand():
-    #"listens for commands"
+    # "listens for commands"
 
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
         speak_command = 'Please give some command to me...'
         talkToMe(speak_command)
-        
+
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
@@ -30,18 +32,18 @@ def myCommand():
     try:
         command = r.recognize_google(audio).lower()
         talkToMe('You said: ' + command + '\n')
-    
-    #loop back to continue to listen for commands if unrecognizable speech is received
+
+    # loop back to continue to listen for commands if unrecognizable speech is received
     except sr.UnknownValueError:
         talkToMe('Your last command couldn\'t be heard ! I can understand commands like send email, open gmail, open website xyz.com and tell me a joke')
-        #speak.Speak('Your last command couldn\'t be heard')
+        # speak.Speak('Your last command couldn\'t be heard')
         command = myCommand()
 
     return command
 
 
 def assistant(command):
-    #"if statements for executing commands"
+    # "if statements for executing commands"
     message = 'Ask me to do something, I am not here for chitchat ! I can understand commands like send email, open gmail, open website xyz.com and tell me a joke'
     if 'hello' in command:
         talkToMe(message)
@@ -51,13 +53,13 @@ def assistant(command):
 
     elif 'hey' in command:
         talkToMe(message)
-    
+
     elif 'austine' in command:
         talkToMe(message)
 
     # open gmail
     elif 'open gmail' in command:
-        #reg_ex = re.search('open gmail (.*)', command)
+        # reg_ex = re.search('open gmail (.*)', command)
         url = 'https://www.gmail.com/'
         webbrowser.open(url)
         talkToMe('Done!')
@@ -72,12 +74,12 @@ def assistant(command):
             talkToMe('Done!')
         else:
             pass
-    
+
     # note pad
     elif 'open notepad' in command:
         os.system('notepad')
         talkToMe('Done for you!')
-    
+
     # what's up
     elif 'whats up' in command:
         talkToMe('Just doing my thing')
@@ -86,12 +88,27 @@ def assistant(command):
     elif 'tell me a joke' in command:
         # fetch from API
         res = requests.get(
-                'https://icanhazdadjoke.com/',
-                headers={"Accept":"application/json"}
-                )
+            'https://icanhazdadjoke.com/',
+            headers={"Accept": "application/json"}
+        )
         # validate https request
         if res.status_code == requests.codes.ok:
             talkToMe('Here is an awesome joke for you- ')
             talkToMe(str(res.json()['joke']))
         else:
             talkToMe('oops!I ran out of jokes')
+
+    #  check current weather
+    #  this option is not funtioning as proxy need to set to bypass HMCL IT settings
+    elif 'current weather in' in command:
+        reg_ex = re.search('current weather in (.+)', command)
+        talkToMe(reg_ex)
+        if reg_ex:
+            city = reg_ex.group(1)
+            weather = Weather()
+            location = weather.lookup_by_location(city)
+            condition = location.condition()
+            talkToMe('The Current weather in %s is %s The temperature is %.1f degree' % (
+                city, condition.text(), (int(condition.temp())-32)/1.8))
+        else:
+            talkToMe("City name not fetched")
